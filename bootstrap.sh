c235f3a1fc1483b1ded41418d42873a03b8b022f
idgserpro <idg@serpro.gov.br>
2020-07-06 16:48:53 -0300

    Fix buildout restart
    Uses https://dist.plone.org/release/5-latest/requirements.txt to avoid
    restarting of buildout

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@
 #     ./bootstrap.sh  # use buildout.cfg
 #     ./bootstrap.sh -c coredev.cfg  # use coredev.cfg
 virtualenv -p python2.7 .
-./bin/pip install -r https://raw.githubusercontent.com/plone/buildout.coredev/5.1/requirements.txt
+./bin/pip install -r https://dist.plone.org/release/5-latest/requirements.txt
 ./bin/buildout "$@"
