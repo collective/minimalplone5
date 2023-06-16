be255953ff211042ee2715bf23ba9199b176d8ff
Johannes Raggam <thetetet@gmail.com>
2023-06-16 12:17:26 +0200

    Use explicit requirements file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@
 #     ./bootstrap.sh  # use buildout.cfg
 #     ./bootstrap.sh -c coredev.cfg  # use coredev.cfg
 virtualenv -p python3 venv
-./venv/bin/pip install -r https://dist.plone.org/release/5-latest/requirements.txt
+./venv/bin/pip install -r requirements.txt
 ./venv/bin/buildout "$@"
