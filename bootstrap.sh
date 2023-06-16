3ab4cf51ba0f00e0425b88aaffe3510a00733df9
Johannes Raggam <thetetet@gmail.com>
2023-06-16 12:16:59 +0200

    Install virtual environment in venv subdirectory.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 # Usage:
 #     ./bootstrap.sh  # use buildout.cfg
 #     ./bootstrap.sh -c coredev.cfg  # use coredev.cfg
-virtualenv -p python3 .
-./bin/pip install -r https://dist.plone.org/release/5-latest/requirements.txt
-./bin/buildout "$@"
+virtualenv -p python3 venv
+./venv/bin/pip install -r https://dist.plone.org/release/5-latest/requirements.txt
+./venv/bin/buildout "$@"
